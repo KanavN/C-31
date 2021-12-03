9a77270354e86f0c12b40dd2be8b4c2a94442275
KanavN <89537335+KanavN@users.noreply.github.com>
2021-12-03 12:54:04 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,22 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeImg = loadImage("sprites/smoke.png");
+    this.trejectory=[]
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    if(this.body.position.x>220 && this.body.velocity.x>10){
+    var position=[this.body.position.x,this.body.position.y];
+    this.trejectory.push(position);
+    }
+    for(var i=0;i<this.trejectory.length;i++)
+    {
+      image(this.smokeImg,this.trejectory[i][0],this.trejectory[i][1]);
+    }
+    super.display();
+  }
+}
